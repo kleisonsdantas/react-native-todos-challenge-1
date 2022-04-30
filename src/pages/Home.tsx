931a9353035e6b7ecfdb9606f0f072139ee450bb
kleisonsdantas <kleisonsdantas@gmail.com>
2022-04-29 21:22:23 -0300

    Challenge Done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,28 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask ={
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => {
+      if(task.id === id) {
+        return {...task, done: !task.done};
+      }
+      return task;
+    })
+
+    setTasks([...updatedTasks]);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks([...tasks.filter((item) => item.id !== id)])
   }
 
   return (
